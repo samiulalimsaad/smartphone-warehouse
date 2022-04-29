67bf8c4cb7186a2c6250897a297f72912c744a0a
samiulalimsaad <saadnbiuraj@gmail.com>
2022-04-30 02:12:56 +0600

    firebase added

---
+++ src/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.VITE_apiKey,
+    authDomain: process.env.VITE_authDomain,
+    projectId: process.env.VITE_projectId,
+    storageBucket: process.env.VITE_storageBucket,
+    messagingSenderId: process.env.VITE_messagingSenderId,
+    appId: process.env.VITE_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    define: {
+        "process.env": env,
+    },
+});
