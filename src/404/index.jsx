b7d7da3d36c5d6829faa0d5795b1c50944c287f5
samiulalimsaad <saadnbiuraj@gmail.com>
2022-04-30 01:07:23 +0600

    added possible routes

---
+++ src/404/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="flex items-center justify-center h-screen font-extrabold text-indigo-600 text-9xl">
+            404 Not Found
+        </div>
+    );
+};
+
+export default NotFound;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./404";
+import Blogs from "./Blogs";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <h1 className="text-6xl text-sky-500">hello</h1>
-        </div>
+        <>
+            <header>Header</header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+            <footer>Footer</footer>
+        </>
     );
 }
 
