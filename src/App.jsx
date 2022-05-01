53ecf2f039b0d58a0cccf928e5be8b18d3786bf1
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-01 16:19:39 +0600

    manage items page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Blogs from "./Blogs";
 import Dashboard from "./Dashboard";
 import Footer from "./Footer";
 import Home from "./Home";
+import Inventory from "./Inventory";
 import Login from "./Login";
+import ManageItems from "./ManageItems";
 import Navbar from "./Navbar";
 import SignUp from "./SignUp";
 
@@ -24,6 +26,8 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/inventory/:id" element={<Inventory />} />
+                    <Route path="/manage-items" element={<ManageItems />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </main>
--- src/ManageItems/index.jsx
+++ src/ManageItems/index.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
 import useTitle from "../utilities/useTitle";
 
 const ManageItems = () => {
     useTitle("Manage Items");
-    return <div>ManageItems</div>;
+    const { id } = useParams();
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get("/data.json").then(({ data }) => {
+            setItems(data.redmi);
+            console.log(data);
+        });
+    }, []);
+
+    return (
+        <section id="inventoryItems" className="container p-20 mx-auto">
+            <h2 className="my-4 text-6xl text-center text-teal-500">
+                Inventory Items
+            </h2>
+            <div className="grid grid-cols-2 gap-4">
+                {items?.map((item) => (
+                    <div
+                        key={item.name}
+                        className="relative w-full rounded-md shadow-md ring-1 ring-slate-300 drop-shadow-md text-slate-800"
+                    >
+                        <div className="flex">
+                            <div className="flex items-center justify-center">
+                                <img
+                                    src={item.images[0]}
+                                    alt=""
+                                    className="object-contain object-center w-auto h-96"
+                                />
+                            </div>
+                            <div className="flex gap-8 p-8 pb-14">
+                                <div>
+                                    <h3 className="text-2xl text-slate-900">
+                                        {item.name}
+                                    </h3>
+                                    <p className="text-xl capitalize">
+                                        Price : ${item.price}
+                                    </p>
+                                    <p className="text-xl capitalize">
+                                        Available : {item.quantity}
+                                    </p>
+                                    <p className="text-xl capitalize">
+                                        supplier Name : {item.supplierName}
+                                    </p>
+                                </div>
+                                <ul className="capitalize text-md text-slate-600">
+                                    {item.description.map((dest) => (
+                                        <li key={dest}>{dest}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="absolute bottom-0 flex w-full">
+                            <NavLink
+                                to={`/inventory/${item.id}`}
+                                className="w-full px-4 py-2 text-center bg-teal-500 rounded-md hover:bg-teal-600 text-slate-50"
+                            >
+                                Stock Update
+                            </NavLink>
+                            <NavLink
+                                to={`/inventory/${item.id}`}
+                                className="w-full px-4 py-2 text-center bg-red-500 rounded-md hover:bg-red-600 text-slate-50"
+                            >
+                                Delete
+                            </NavLink>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default ManageItems;
