5bc1c054844c3bb77ae1e2854a040b26b339b82f
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-02 13:12:17 +0600

    loading component added

--- src/Loading/index.jsx
+++ src/Loading/index.jsx
@@ -4,7 +4,7 @@ import useTitle from "../utilities/useTitle";
 const Loading = () => {
     useTitle("Loading");
     return (
-        <div className="flex items-center justify-center h-screen animate-bounce">
+        <div className="flex items-center justify-center h-full animate-bounce">
             <h2 className="my-4 text-6xl text-center text-teal-500">Loading</h2>
             <div className="flex items-center justify-center ml-2 space-x-2">
                 <div className="w-6 h-6 bg-teal-700 rounded-full" />
